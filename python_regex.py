3491576a7ca94c84c730e73424bdd77a686f72a4
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-08-07 13:05:33 -0500

    Python: Refactoriza expresión regular, crea segunda agrupación que muestra encuentros amistosos

--- python_regex.py
+++ python_regex.py
@@ -3,7 +3,7 @@
 import re                                       # Importa Líbrería Regex
 
 # Usamos el comando r (match) para validar nuestra expresión que en Python se encierra entre ' comillas sencillas
-pattern = re .compile( r'^([\d]{4,4})\-.*$' )
+pattern = re .compile( r'^([\d]{4,4})\-[\d]{2}\-[\d]{2},(.*),Friendly.*$' )
 
 # Abre el archivo
 file = open( "./files/results.csv", "r" )
@@ -12,7 +12,7 @@ file = open( "./files/results.csv", "r" )
 for line in file:
     res = re .match( pattern, line )            # Valida el patrón (previamente compilado que hace la ejecución más rápida)
     if res:                                     # Valida si la expresión ha hecho match
-        print "%s \n" % res .group( 1 )
+        print( "{}: {}\n" .format( res .group( 1 ), res .group( 2 ) ) )
 
 # Cierra el archivo
 file .close()
