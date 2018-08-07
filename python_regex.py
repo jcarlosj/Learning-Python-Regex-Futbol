012f20cae090ea24c5b8e1c5ee3284aed47666aa
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-08-07 13:52:51 -0500

    Python: Refactoriza expresión regular, para mostrar los encuentros con más de 10 goles

--- python_regex.py
+++ python_regex.py
@@ -3,7 +3,7 @@
 import re                                       # Importa Líbrería Regex
 
 # Usamos el comando r (match) para validar nuestra expresión que en Python se encierra entre ' comillas sencillas
-pattern = re .compile( r'^([\d]{4,4})\-[\d]{2}\-[\d]{2},(.*),Friendly.*$' )
+pattern = re .compile( r'^([\d]{4,4})\-[\d]{2}\-[\d]{2},(.+),(.+),(\d+),(\d+),.*$' )
 
 # Abre el archivo
 file = open( "./files/results.csv", "r" )
@@ -12,7 +12,10 @@ file = open( "./files/results.csv", "r" )
 for line in file:
     res = re .match( pattern, line )            # Valida el patrón (previamente compilado que hace la ejecución más rápida)
     if res:                                     # Valida si la expresión ha hecho match
-        print( "{}: {}\n" .format( res .group( 1 ), res .group( 2 ) ) )
+        total = int( res .group( 4 ) ) + int( res .group( 5 ) )
+        if 10 < total:
+            print "Goles: %d, (%s): | %s Vs %s [ %s - %s ]" % \
+                ( total, res .group( 1 ), res .group( 2 ), res .group( 3 ), res .group( 4 ), res .group( 5 ) )
 
 # Cierra el archivo
 file .close()
