ea6d914367971bd1f93fa4f23af71ce58cdabf6d
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-08-07 12:44:31 -0500

    Python: Crea expresión regular y agrupa año para imprimir todos los años del listado

--- python_regex.py
+++ python_regex.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import re                                       # Importa Líbrería Regex
+
+# Usamos el comando r (match) para validar nuestra expresión que en Python se encierra entre ' comillas sencillas
+pattern = re .compile( r'^([\d]{4,4})\-.*$' )
 
 # Abre el archivo
 file = open( "./files/results.csv", "r" )
 
 # Itera cada uno de las líneas del archivo
 for line in file:
-    print line
+    res = re .match( pattern, line )            # Valida el patrón (previamente compilado que hace la ejecución más rápida)
+    if res:                                     # Valida si la expresión ha hecho match
+        print "%s \n" % res .group( 1 )
 
 # Cierra el archivo
 file .close()
